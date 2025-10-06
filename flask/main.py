ef25ee8f55f336a2877e9e5b9080db15eb8a0ee6
DS7 <p.danielsalvador@gmail.com>
2025-10-06 15:20:01 -0300

    Prova

---
+++ flask/main.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route('/soma/valor1=<int:n1>/valor2=<int:n2>')
+def soma(n1,n2):
+    return "resultado da soma é {}".format(n1 + n2)
+
+@app.route('/divisao/valor1=<int:n1>/valor2=<int:n2>')
+def div(n1,n2):
+    
+    if n2 == 0:
+        return f'Está invalido'
+    else:
+        return "resultado da divisão é {}".format(n1 / n2)
+    
+@app.route('/multiplicar/valor1=<int:n1>/valor2=<int:n2>')
+def mult(n1,n2):
+    
+    return "resultado da multiplição é {}".format(n1 * n2)
+
+@app.route('/subtrair/valor1=<int:n1>/valor2=<int:n2>')
+def sub(n1,n2):
+    
+    return "resultado da subtração é {}".format(n1 - n2)
+
+if __name__ == "__main__":
+    app.run(debug=True)
